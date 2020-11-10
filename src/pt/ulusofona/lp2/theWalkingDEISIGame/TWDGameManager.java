16637e01ee2083d8d9e08ff28e4a7258108c8561
Lucas Souza <lucasyansouza@hotmail.com>
2020-11-10 19:18:08 +0000

    Métodos e Construtores obrigatórios

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -1,4 +1,64 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TWDGameManager {
+
+    public TWDGameManager () {
+    }
+
+    public boolean startGame(File ficheiroInicial) {
+        return true;
+    }
+
+    public int[] getWorldSize() {
+        return new int[0];
+    }
+
+    public int getInitialTeam() {
+        return 0;
+    }
+
+    public List<Humano> getHumans() {
+        return new ArrayList<>();
+    }
+
+    public List<Zombie> getZombies() {
+        return new ArrayList<>();
+    }
+
+    public boolean move(int xO, int yO, int xD, int yD) {
+        return true;
+    }
+
+    public boolean gameIsOver() {
+        return true;
+    }
+
+    public List<String> getAuthors() {
+        return new ArrayList<>();
+    }
+
+    public int getCurrentTeamId() {
+        return 0;
+    }
+
+    public int getElementId(int x, int y) {
+        return 0;
+    }
+
+    public List<String> getSurvivors() {
+        return new ArrayList<>();
+    }
+
+    public boolean isDay() {
+        return true;
+    }
+
+    public boolean hasEquipment(int creatureId, int equipmentTypeId) {
+        return true;
+    }
+
 }
--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
@@ -1,4 +1,16 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
 public class Zombie {
+
+    public int getId() {
+        return 0;
+    }
+
+    public String getImagePNG() {
+        return "test";
+    }
+
+    public String toString() {
+        return "test";
+    }
 }
