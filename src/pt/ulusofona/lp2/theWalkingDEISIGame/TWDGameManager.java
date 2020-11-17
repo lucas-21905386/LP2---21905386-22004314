a74b408a8044092ceb220b4ddb8d27415381fb26
Slaingerz <32557930+Slaingerz@users.noreply.github.com>
2020-11-17 04:13:27 +0000

    Start Game

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -17,52 +17,52 @@ public class TWDGameManager {
     public TWDGameManager () {
     }
 
-    public boolean startGame(File ficheiroInicial) throws IOException {
+    public boolean startGame(File ficheiroInicial) {
         int count = 1;
-        Scanner ficheiro = new Scanner(ficheiroInicial);
-        String linha = ficheiro.nextLine();
-        String[] linhaInfo = linha.split(" ");
-        grid = new int[]{Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1])};
-        while (ficheiro.hasNextLine()) {
-            linha = ficheiro.nextLine();
-            count++;
-            linhaInfo = linha.split(" : ");
-            switch (linhaInfo.length) {
-                case 1: {
-                    if (count == 2) {
-                        first = Integer.parseInt(linhaInfo[0]);
-                    } else if (count == 3) {
-                        totalCriaturas = Integer.parseInt(linhaInfo[0]);
-                    } else {
-                        totalEquipamentos = Integer.parseInt(linhaInfo[0]);
+        try {
+            Scanner ficheiro = new Scanner(ficheiroInicial);
+            String linha = ficheiro.nextLine();
+            String[] linhaInfo = linha.split(" ");
+            grid = new int[]{Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1])};
+            while (ficheiro.hasNextLine()) {
+                linha = ficheiro.nextLine();
+                count++;
+                linhaInfo = linha.split(" : ");
+                switch (linhaInfo.length) {
+                    case 1: {
+                        if (count == 2) {
+                            first = Integer.parseInt(linhaInfo[0]);
+                        } else if (count == 3) {
+                            totalCriaturas = Integer.parseInt(linhaInfo[0]);
+                        } else {
+                            totalEquipamentos = Integer.parseInt(linhaInfo[0]);
+                        }
                     }
-                }
-                break;
-                case 4: {
-                    equipamentos.add(new Equipamentos(Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1]),
-                            Integer.parseInt(linhaInfo[2]), Integer.parseInt(linhaInfo[3])));
-                }
-                break;
-                case 5: {
-                    if (Integer.parseInt(linhaInfo[1]) == 0) {
-                        zombies.add(new Zombie(Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1]),
-                                linhaInfo[2], Integer.parseInt(linhaInfo[3]), Integer.parseInt(linhaInfo[4])));
+                    break;
+                    case 4: {
+                        equipamentos.add(new Equipamentos(Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1]),
+                                Integer.parseInt(linhaInfo[2]), Integer.parseInt(linhaInfo[3])));
                     }
-                    if (Integer.parseInt(linhaInfo[1]) == 1) {
-                        humanos.add(new Humano(Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1]),
-                                linhaInfo[2], Integer.parseInt(linhaInfo[3]), Integer.parseInt(linhaInfo[4])));
+                    break;
+                    case 5: {
+                        if (Integer.parseInt(linhaInfo[1]) == 0) {
+                            zombies.add(new Zombie(Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1]),
+                                    linhaInfo[2], Integer.parseInt(linhaInfo[3]), Integer.parseInt(linhaInfo[4])));
+                        }
+                        if (Integer.parseInt(linhaInfo[1]) == 1) {
+                            humanos.add(new Humano(Integer.parseInt(linhaInfo[0]), Integer.parseInt(linhaInfo[1]),
+                                    linhaInfo[2], Integer.parseInt(linhaInfo[3]), Integer.parseInt(linhaInfo[4])));
+                        }
                     }
+                    break;
+                    default:
                 }
-                break;
-                default:
             }
-        }
-        ficheiro.close();
-        if (count == 4 + totalCriaturas + totalEquipamentos) {
-            return true;
-        } else {
+        } catch (IOException e) {
+            e.printStackTrace();
             return false;
         }
+        return true;
     }
 
     public int[] getWorldSize() {
