c930a582d99d611f7fbb588bf4402632e546ebed
Slaingerz <32557930+Slaingerz@users.noreply.github.com>
2021-01-01 11:58:40 +0000

    Class Creature

---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Creature.java
@@ -0,0 +1,102 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+public abstract class Creature {
+    private int iD, iDTipo, x, y, contarEquip;
+    private String nome, nomeEquipa;
+
+    Creature (int iD, int iDTipo, int x, int y, String nome) {
+        this.iD = iD;
+        this.iDTipo = iDTipo;
+        this.x = x;
+        this.y = y;
+        this.nome = nome;
+        if (iDTipo > 4) {
+            setNomeEquipa("Os Vivos");
+        } else {
+            setNomeEquipa("Os Outros");
+        }
+    }
+
+    public int getId() {
+        return iD;
+    }
+
+    public String tipoById(int iDTipo) {
+        switch (iDTipo) {
+            case 0: return "Criança (Zombie)";
+            case 1: return "Adulto (Zombie)";
+            case 2: return "Militar (Zombie)";
+            case 3: return "Idoso (Zombie)";
+            case 4: return "Zombie Vampiro";
+            case 5: return "Criança (Vivo)";
+            case 6: return "Adulto (Vivo)";
+            case 7: return "Militar (Vivo)";
+            case 8: return "Idoso (Vivo)";
+            case 9: return "Cão";
+            default: return "Desconhecido";
+        }
+    }
+
+    public String getImagePNG() {
+        return "";
+    }
+
+    public String toString(){
+        return iD + " | " + tipoById(iDTipo) + " | " + nomeEquipa + " | " + nome + " " + contarEquip +
+                " " + "@" + " " + "(" + x + ", " + y + ")";
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public int getiDTipo() {
+        return iDTipo;
+    }
+
+    public void setiDTipo(int iDTipo) {
+        this.iDTipo = iDTipo;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getContarEquip() {
+        return contarEquip;
+    }
+
+    public void setContarEquip() {
+        this.contarEquip++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeEquipa() {
+        return nomeEquipa;
+    }
+
+    public void setNomeEquipa(String nomeEquipa) {
+        this.nomeEquipa = nomeEquipa;
+    }
+
+
+}
