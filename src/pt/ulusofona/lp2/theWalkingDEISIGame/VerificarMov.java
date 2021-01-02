09d99372ec3d88f8e27647c1752de070fd0d4eb7
migue <ASdrobal2001>
2021-01-02 20:27:06 +0000

    Criação criação do ficheiro VerificarMov.java com a class VerificarMov com o public crianca , adulto , militar , idoso , cao , vampiro

---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/VerificarMov.java
@@ -0,0 +1,55 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+public class VerificarMov {
+    public boolean crianca(int xO, int yO, int xD, int yD) {
+        if (xD != xO && yD != yO || xD - xO > 1 || xD - xO < -1 || yD - yO > 1 || yD - yO < -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean adulto(int xO, int yO, int xD, int yD) {
+        if (xD - xO > 2 || xD - xO < -2 || yD - yO > 2 || yD - yO < -2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean militar(int xO, int yO, int xD, int yD) {
+        if (xD - xO > 3 || xD - xO < -3 || yD - yO > 3 || yD - yO < -3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean idoso(int xO, int yO, int xD, int yD, int tipo) {
+        if (tipo == 8) {
+            if (xD != xO && yD != yO || xD - xO > 1 || xD - xO < -1 || yD - yO > 1 || yD - yO < -1 || !new TWDGameManager().isDay()) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return crianca(xO, yO, xD, yD);
+        }
+    }
+
+    public boolean cao(int xO, int yO, int xD, int yD) {
+        if (xD == xO || yD == yO || xD + yD > xO + yO + 4) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean vampiro(int xO, int yO, int xD, int yD) {
+        if (xD - xO > 2 || xD - xO < -2 || yD - yO > 2 || yD - yO < -2 || new TWDGameManager().isDay()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
