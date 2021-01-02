23a9736f6db5943750977269583ae60ff16f9c77
Miguel-22004314 <a22004314@alunos.ulht.pt>
2021-01-02 15:25:50 +0000

    Atualização atualização zombie

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Zombie.java
@@ -1,53 +1,7 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
-public class Zombie {
-    private int iD, iDTipo, equipDestruidos, x, y;
-    private String nome, nomeEquipa;
-
+public class Zombie extends Creature {
     Zombie (int iD, int iDTipo, String nome, int x, int y) {
-        this.iD = iD;
-        this.iDTipo = iDTipo;
-        this.nome = nome;
-        this.x = x;
-        this.y = y;
-        this.equipDestruidos = 0;
-        this.nomeEquipa = "Os Outros";
-    }
-
-    public int getId() {
-        return this.iD;
-    }
-
-    public String getImagePNG() {
-        return "zombie.png";
-    }
-
-    public String toString() {
-        return iD + " | Zombie | " + nomeEquipa + " | " + nome +
-                " " + equipDestruidos + " @ (" + x + ", " + y +")";
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setEquipDestruidos() {
-        this.equipDestruidos++;
+        super(iD, iDTipo, x, y, nome);
     }
 }
