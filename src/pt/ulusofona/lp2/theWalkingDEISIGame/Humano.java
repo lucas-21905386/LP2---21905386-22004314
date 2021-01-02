41d6f464b6eb90bf9e159394ca5e2822ad363113
migue <ASdrobal2001>
2021-01-02 16:11:25 +0000

    Atualização atualização Humano

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Humano.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Humano.java
@@ -1,44 +1,10 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
-public class Humano {
-    private int iD, iDTipo, equipApanhados, x, y;
-    private String nome, nomeEquipa;
+public class Humano extends Creature {
     private Equipamentos equip;
 
     Humano (int iD, int iDTipo, String nome, int x, int y) {
-        this.iD = iD;
-        this.iDTipo = iDTipo;
-        this.nome = nome;
-        this.x = x;
-        this.y = y;
-        this.equipApanhados = 0;
-        this.nomeEquipa = "Os Vivos";
-        this.equip = null;
-    }
-
-    public int getId() {
-        return this.iD;
-    }
-
-    public String getImagePNG() {
-        return "human.png";
-    }
-
-    public String toString() {
-        return iD + " | Humano | " + nomeEquipa + " | " + nome +
-                " " + equipApanhados + " @ (" + x + ", " + y +")";
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public String getNome() {
-        return nome;
+        super(iD, iDTipo, x, y, nome);
     }
 
     public Equipamentos getEquip() {
@@ -47,17 +13,5 @@ public class Humano {
 
     public void setEquip(Equipamentos equip) {
         this.equip = equip;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setEquipApanhados() {
-        this.equipApanhados++;
     }
 }
